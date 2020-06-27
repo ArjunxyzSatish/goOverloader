395c8f59a9a8962c35eae20ba862a1f58f7f12c7
Arjun Satish <arjunsatish@protonmail.com>
2020-06-27 19:28:00 +0530

    Finished

---
+++ overloader.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+)
+
+
+func main() {
+
+	for true{
+		var filename string
+		filename = strconv.Itoa(rand.Int())
+		fmt.Println("Creating file ", filename)
+		os.Create(filename)
+	}
+}
+
